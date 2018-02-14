b339dc3001a143936559ea56e4c579c6723c2faf
André Aparício <aparicio99@gmail.com>
2018-02-14 00:20:37 +0000

    Add timezone config option

--- getip.php
+++ getip.php
@@ -5,13 +5,14 @@ require('../config.php');
 
 function print_table_last_ip($table) {
 	global $DB_FILE;
+	global $TIMEZONE;
 
 	$db = new SQLite3($DB_FILE);
 	$results = $db->query('SELECT max(id) as id, ip, timestamp FROM '.$table.';');
 	$row = $results->fetchArray();
 
 	$date = new DateTime();
-	$date->setTimeZone(new DateTimeZone('Europe/Lisbon'));
+	$date->setTimeZone(new DateTimeZone($TIMEZONE));
 	$date->setTimestamp($row['timestamp']);
 	$date_string = $date->format('Y-m-d H:i:s');
 
